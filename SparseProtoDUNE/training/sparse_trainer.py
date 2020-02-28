c37b694cd64e0ad66f25e603bbce27b5520d9ffa
csarasty <sarastce@mail.uc.edu>
2020-02-28 17:20:04 -0500

    nan fix for the loss function

--- SparseProtoDUNE/training/sparse_trainer.py
+++ SparseProtoDUNE/training/sparse_trainer.py
@@ -22,11 +22,13 @@ def categorical_cross_entropy(y_pred, y_true):
   y_pred = torch.clamp(y_pred, 1e-9, 1 - 1e-9)
   # Normalise the loss!
   loss = -(y_true * torch.log(y_pred))
-  weights = y_true.shape[0]/(y_true.shape[1]*y_true.sum(dim=0))
-  #print(weights)
-  weighted_loss = weights[None,:] * loss
+  weights = torch.zeros(y_true.shape[1]).to(y_true.device)
+  class_sum = y_true.sum(dim=0)
+  mask = (class_sum>0)
+  weights[mask] = y_true[:,mask].shape[0]/(y_true.shape[1]*class_sum[mask])
+  weighted_loss =  weights[None,:] * loss
   return weighted_loss.sum(dim=1).mean()
-  #return -(y_true * torch.log(y_pred)).sum(dim=1).mean()
+  #return -(y_true * torch.log(y_pred)).sum(dim=1).mean()i
 
 class SparseTrainer(base):
   '''Trainer code for basic classification problems with categorical cross entropy.'''

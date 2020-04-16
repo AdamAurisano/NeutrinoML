d1f9be6603bc53137f67dbe14028eb1dc61eb0d6
Haejun-Oh <ohhu@mail.uc.edu>
2020-04-16 17:08:23 -0400

    Added a function to alias enum interaction index to four flavor indexes

--- SparseNOvA/datasets/spm_nova.py
+++ SparseNOvA/datasets/spm_nova.py
@@ -30,35 +30,6 @@ class SparsePixelMapNOvA(Dataset):
         self.total_events = 0
         for val in self.file_metadata:
             self.total_events += len(val['mask'])
-
-        # print(self.total_events)
-        # print(self.file_metadata[2]['mask'])
-
-        # Here we can do a glob to get a list of all file names
-        # We can then open each of those files and count the number of images
-        # to count up how many training images we have in total
-        #
-        # In python you can set any object as a class variable, meaning
-        # it will not be discarded at the end of the function call, and
-        # will remain as long as the object exists. So if I did
-        #
-        #      n_files = 0
-        #
-        # then n_files will go out of scope and be lost when the __init__
-        # function returns. However, if I instead do
-        #
-        #      self.n_files = 0
-        #
-        # then n_files will be assigned to the dataset object, and
-        # persists after __init__ returns. If we initialise a SparsePixelMap
-        # object, we can do
-        #
-        #      dataset = SparsePixelMapNOvA(filedir=/path/to/files)
-        #      print(dataset.n_files)
-        #
-        # By declaring n_files with the "self." prefix, we've declared it as
-        # what's called a "data attribute" of the class - an instance of this
-        # class will hold onto it for as long as that instance exists
         
     def __len__(self):
         return self.total_events
@@ -67,6 +38,7 @@ class SparsePixelMapNOvA(Dataset):
         '''Return training information at provided index'''
         if not 0 <= idx < self.total_events:
             raise Exception(f'Event number {idx} invalid – must be in range 0 -> {self.total_events-1}.')
+            
         # First step: find the file the neutrino event is in.
         counter = idx
         event_number = 0
@@ -94,11 +66,12 @@ class SparsePixelMapNOvA(Dataset):
         # j tells you the position in the file
         j = self.file_metadata[i]['mask'][counter]
         truth = self.file_metadata[i]['truth'][counter]
-#         print(j)
+        new_truth = self.get_alias(truth)
         image = self.current_file[1]['rec.training.slicemaps']['slicemap'][j]
         xview, yview = image.reshape(2, 448, 384)[:]
         data = { 'xview': utils.dense_to_sparse(xview),
-                 'yview': utils.dense_to_sparse(yview) }
+                 'yview': utils.dense_to_sparse(yview), 
+                 'truth': new_truth}
 
         return data
     
@@ -123,7 +96,7 @@ class SparsePixelMapNOvA(Dataset):
                 raise Exception(f'Mismatch found: {len(mask)} images and {len(truth)} truths.')
             self.file_metadata.append({ 'mask': mask, 'truth': truth })
             file.close()
-        return 
+        return
     
     def match_truth(self, file, im_idx):
         '''Returns list of index locations of trainingdata for a given slicemap'''
@@ -138,49 +111,19 @@ class SparsePixelMapNOvA(Dataset):
             raise Exception(f'{len(true_idx)} truths found for slicemap {im_idx}.')
         return true_idx
 
-    #         # situation 1 to open/close file
-    #         if self.current_file[0] is not None and self.current_file[0] != event_file:
-    #             self.current_file[1].close()
-    #             self.current_file = (event_file, h5py.File(self.files[event_file]))
-    #             self.current_file.open()
-    #             # Check to see if there is an open file beforehand.
-
-    #         # situation 2 to open/close file
-    #         else:
-    #             # situation 3 to open/close file
-    #             if self.current_file[0] is None:
-    #                 self.current_file = (event_file, h5py.File(self.files[event_file]))
-    #                 self.current_file.open()
-    #             # situation 4 to open/close file
-    #             elif self.current_file[0] == event_file:
-    #                 pass
-              
-    # This is where most of the work needs to be done. Say we have 10 files,
-    # each containing 10,000 images – 100,000 images total.
-    # This function takes in an integer in the 0-99999 range (and should throw
-    # an error if it's outside that range). The function needs to know how many
-    # files there are, and how many images each one contains. It needs to know
-    # that image number 15,000 is actually image 5,000 in file 2, and then
-    # open up that file and extract the corresponding pixel map.
-    #
-    # It should minimise the opening and closing of files. When we open a file,
-    # we can hold onto it and only close it when necessary. For file i, we can
-    # do
-    #
-    # self.current_file = (i, h5py.File(self.files[i]))
-    #
-    # to open the file, and keep hold of it. current_file is a tuple - basically
-    # a two-element array, where current_file[0] is the file number and
-    # current_file[1] holds the file itself. Then the next time we load an image
-    # for file i:
-    #
-    # if (i != self.current_file[0]):
-    #   self.current_file.close()
-    #   self.current_file = (i, h5py.File(self.files[i]))
-    #
-    # so if file i is already open, we skip the open/close part and go straight to
-    # loading the image.
-    #
-    # The first step is just getting the bookkeeping right here. Once that's
-    # done, we can add in a few more minor things like transforming the
-    # pixel maps into PyTorch tensors in a sparse format.
+    def get_alias(self, flav):
+        '''Function that alias interaction enum to the following list'''
+        # [0 == nu_mu, 1 == nu_e, 2 == nu_tau, 3 == NC, 4 == others]
+        if 0 <= flav < 4:
+            flav = 0
+        elif 4 <= flav < 8:
+            flav = 1
+        elif 8 <= flav < 12:
+            flav = 2
+        elif flav == 13:
+            flav = 3
+        else:
+            flav = 4
+#         maybe fifth class
+        return flav
+            

a96e5d949503088b69bd6ddb9e0201d24ba1c288
jethewes <39996356+jethewes@users.noreply.github.com>
2020-07-27 15:03:15 -0500

    Update run_docker_pytorch.sh
    Update ProtoDUNE docker script to mount entire repository

--- SparseProtoDUNE/scripts/run_docker_pytorch.sh
+++ SparseProtoDUNE/scripts/run_docker_pytorch.sh
@@ -6,8 +6,9 @@ if [ "$1" == "" ]; then
 fi
 
 # Get script directory
-scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-topdir=`dirname $scriptdir`
+topdir=/home/$USER/NeutrinoML
+#scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#topdir=`dirname $scriptdir`
 
 # Get docker port prefix based on username
 if   [ "$USER" == "hewesje"  ]; then

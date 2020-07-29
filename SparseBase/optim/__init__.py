2a2dd5f7cb14d5823b81d24534f4c1485cdcb531
CarlosSarasty <sarastce.mail.uc.edu>
2020-07-29 01:06:06 -0400

    fixing typos

--- SparseBase/optim/__init__.py
+++ SparseBase/optim/__init__.py
@@ -7,6 +7,6 @@ def get_optim(model_params, optimizer, **params):
   # Not hooking in Ranger just yet, but we should
   if True:
     import torch.optim
-    return gettattr(torch.optim, optimizer)(
-      model_params, **params[optimizer])
+    return getattr(torch.optim, optimizer)(
+            model_params, **params[optimizer])
 

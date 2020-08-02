eed11fd2a68d27922a444b41aaa5e36f2a377dd6
Haejun Oh <ohhu@mail.uc.edu>
2020-08-02 05:52:36 +0000

    created a new function that arranges the format of data depending on Sparse or Minkowski Network

---
+++ Core/utils/arrange_data.py
@@ -0,0 +1,16 @@
+import torch, MinkowskiEngine as ME
+
+def arrange_sparse(data, device):
+    batch_input = (data['c'].to(device), data['x'].to(device), batch_size)
+    return batch_input
+
+def arrange_sparse_minkowski(data, device):
+    batch_input = data['sparse'].to(device)
+    return batch_input
+
+def arrange_sparse_minkowski_2stack(data, device):
+    batch_input = [data['sparse'][0].to(device),
+                   data['sparse'][1],   
+                   data['sparse'][2].to(device),
+                   data['sparse'][3] ]
+    return batch_input

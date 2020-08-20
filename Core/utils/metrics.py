82756ed424f4fca83becff85574fef0863f80061
naporana <naporana@mail.uc.edu>
2020-08-20 19:07:12 -0400

    fixed model name to MinkowskiSeg

--- Core/utils/metrics.py
+++ Core/utils/metrics.py
@@ -2,16 +2,21 @@
 Functions for calculating training metrics
 '''
 
-def metrics_semantic_segmentation(y_true, y_pred, class_names=None):
+def metrics_semantic_segmentation(batch_target, batch_output, class_names=None):
     '''Function to calculate training metrics for semantic segmentation'''
     metrics = { 'batch': {}, 'epoch': {} }
     w_pred = batch_output.argmax(dim=1)
-    w_true = batch_target
+    w_true = batch_target.argmax(dim=1)
     correct = (w_pred==w_true)
     metrics['batch']['Acc/batch'] = 100*correct.sum().float().item()/w_pred.shape[0]
     # Calculate accuracy individually for each class, if requested
     if class_names is not None:
         if len(class_names) != batch_output.shape[1]:
           raise Exception(f'Number of class names ({len(class_names)}) does not match shape of network output ({batch_output.shape[1]})!')
-        #metrics['IndivAcc/acc_indiv = [ 100*((w_pred[correct]==i).sum().float()/(w_true==i).sum().float()).item() for i in range(batch_target.shape[1]) ]
+        else:
+            metrics['batch']['IndivAcc/acc_indiv'] = [ 100*((w_pred[correct]==i).sum().float()/(w_true==i).sum().float()).item()\
+                for i in range(batch_target.shape[1]) ]
+    return metrics
+
+
 
--- SparseProtoDUNE/datasets/SegTruth.py
+++ SparseProtoDUNE/datasets/SegTruth.py
@@ -6,7 +6,7 @@ def GetShowers(pdg, trackId):
   em_particles = [ abs(it1_id) for it2_pdg, it2_id in zip(pdg, trackId) for it1_pdg, it1_id in zip(it2_pdg, it2_id) if abs(it1_pdg) == 11 or it1_pdg == 22 ]
   unique = set(em_particles)
   count = { u: em_particles.count(u) for u in unique }
-  return tuple(key for key, val in count.items() if val > 55)
+  return tuple(key for key, val in count.items() if val > 0)
 
 def SegTruth(pdg, trackId, process, energy):
 
@@ -17,32 +17,37 @@ def SegTruth(pdg, trackId, process, energy):
   # Figure out which EM hits should be considered showers
   showers = GetShowers(pdg, trackId)
 
+  #['shower','delta','diffuse','kaons','michel','hip','mu','pi'] 
   for idx in range(len(pdg)):
-    for jdx in range(len(pdg[idx])):
-      # Showers
-      if abs(trackId[idx][jdx]) in showers: 
-      # Electron shower 
-        if (abs(pdg[idx][jdx]) == 11):
-          y_true[idx,0] += energy[idx][jdx]
-          proc[idx,0] = str(process[idx][jdx])
-        # Photon shower
-        elif pdg[idx][jdx] == 22:
-          y_true[idx,1] += energy[idx][jdx] 
-          proc[idx,1] = str(process[idx][jdx])
-        else:
-          y_true[idx,2] += energy[idx][jdx] # Diffuse energy deposition if ten or fewer hits
+    if len(pdg[idx]) > 0:
+      for jdx in range(len(pdg[idx])): #loop over each pixel 
+        # Showers
+        if abs(pdg[idx][jdx]) == 11:  
+          if abs(trackId[idx][jdx]) in showers:
+            if process[idx][jdx]==b'phot' or process[idx][jdx]==b'eIoni' or process[idx][jdx]==b'conv' or process[idx][jdx]==b'compt':
+              y_true[idx,0] += energy[idx][jdx]
+              proc[idx,0] = str(process[idx][jdx])
+        # Delta rays
+            elif process[idx][jdx]==b'muIoni':
+              y_true[idx,1] += energy[idx][jdx]
+              proc[idx,1] =  str(process[idx][jdx])
+        # Diffuse energy depositions
+        #    else: 
+        #      y_true[idx,2] += energy[idx][jdx] # Diffuse energy deposition if 20 or fewer hits
+        #      proc[idx,2] = str(process[idx][jdx])
+        elif process[idx][jdx] == b'neutronInelastic' or process[idx][jdx] == b'nCapture':
+          y_true[idx,2] += energy[idx][jdx]
           proc[idx,2] = str(process[idx][jdx])
-      # Delta rays
-      elif trackId[idx][jdx] < 0 and (abs(pdg[idx][jdx]) == 13 or abs(pdg[idx][jdx]) ==211): # Delta rays 
-        y_true[idx,3] += energy[idx][jdx]
-        proc[idx,3] =  str(process[idx][jdx])
-      # Diffuse energy depositions
-      elif trackId[idx][jdx] < 0 or process[idx][jdx] == b'neutronInelastic' or process[idx][jdx] == b'nCapture':
-        y_true[idx,2] += energy[idx][jdx]
-        proc[idx,3] = str(process[idx][jdx])
-      elif trackId[idx][jdx] > 0:
+        # Delta rays
+        elif pdg[idx][jdx] == 97:
+          y_true[idx,1] += energy[idx][jdx]
+          proc[idx,1] =  str(process[idx][jdx])
+        # kaons 
+        elif abs(pdg[idx][jdx]) == 321:
+          y_true[idx,3] += energy[idx][jdx]
+          proc[idx,3] =  str(process[idx][jdx])
         # Michel electrons + positrons
-        if pdg[idx][jdx] == 98 or pdg[idx][jdx] == 99:
+        elif pdg[idx][jdx] == 98 or pdg[idx][jdx] == 99:
           y_true[idx,4] += energy[idx][jdx]
           proc[idx,4] = str(process[idx][jdx])
         # Highly ionising particles
@@ -57,11 +62,8 @@ def SegTruth(pdg, trackId, process, energy):
         elif abs(pdg[idx][jdx]) == 211:
           y_true[idx,7] += energy[idx][jdx]
           proc[idx,7] = str(process[idx][jdx])
-        elif abs(pdg[idx][jdx]) == 11:
-          y_true[idx,2] += energy[idx][jdx]
-          proc[idx,2] = str(process[idx][jdx])
-        else: raise Exception('Unrecognised particle found in ground truth!', f'Truth information not recognised! Particle is {Particle.from_pdgid(pdg[idx][jdx]).name} ({pdg[idx][jdx]})')
-      else: raise Exception('Unrecognised particle found in ground truth!', f'Truth information not recognised! PDG is {Particle.from_pdgid(pdg[idx][jdx]).name} ({pdg[idx][jdx]})')
+       # else: raise Exception('Unrecognised particle found in ground truth!', f'Truth information not recognised! Particle is {Particle.from_pdgid(pdg[idx][jdx]).name} ({pdg[idx][jdx]})')
+      #else: raise Exception('Unrecognised particle found in ground truth!', f'Truth information not recognised! PDG is {Particle.from_pdgid(pdg[idx][jdx]).name} ({pdg[idx][jdx]})')
 
   return noise_mask, y_true, proc 
 

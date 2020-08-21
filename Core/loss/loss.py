cbd6bd1de365a2adc8d6106791d2ee097cba6ccb
j hewes <jhewes15@fnal.gov>
2020-08-21 17:57:10 +0000

    Removed verbose output in loss function

--- Core/loss/loss.py
+++ Core/loss/loss.py
@@ -5,12 +5,8 @@ def categorical_cross_entropy(y_pred, y_true):
   # Normalise the loss!
   loss = -(y_true * torch.log(y_pred))
   weights = torch.zeros(y_true.shape[1]).to(y_true.device)
-  print(y_true)
-  print(y_pred)
   class_sum = y_true.sum(dim=0)
   mask = (class_sum>0)
   weights[mask] = y_true[:,mask].shape[0]/(y_true.shape[1]*class_sum[mask])
-  print('weights', weights[mask])
-  print('sum class', class_sum)
   weighted_loss = weights[None,:] * loss
   return weighted_loss.sum(dim=1).mean()

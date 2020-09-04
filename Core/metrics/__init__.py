b8995bf511ec8e2c448bc8776bed93ea0097933a
jeremy hewes <jhewes15@fnal.gov>
2020-09-04 10:42:04 -0400

    Added new metric calculation code for classification network

--- Core/metrics/__init__.py
+++ Core/metrics/__init__.py
@@ -6,6 +6,9 @@ def get_metrics(metrics):
     if metrics == 'SemanticSegmentation':
         from .semantic_segmentation import SemanticSegmentationMetrics
         return SemanticSegmentationMetrics
+    elif metrics == 'Classification':
+        from .classification import ClassificationMetrics
+        return ClassificationMetrics
     else:
         raise Exception(f'Metric class "{metrics}" not recognised!')
 

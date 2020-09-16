90de74e40d05dd61fd8069c242ab5d51b05bb3b0
CarlosSarasty <sarastce@mail.uc.edu>
2020-09-16 17:05:43 +0000

    update /Core/loss/__init__.py

--- Core/loss/__init__.py
+++ Core/loss/__init__.py
@@ -6,6 +6,9 @@ def get_loss(loss, **params):
   if loss == 'categorical_cross_entropy':
     from .loss import categorical_cross_entropy
     return categorical_cross_entropy
+  if loss == 'generalized_dice':
+    from .loss import generalized_dice
+    return generalized_dice
   else:
     from torch import nn
     return getattr(nn.modules.loss, loss)(**params)

782dbabb64360eb7aeddfdfcc85819efe36bf036
CarlosSarasty <sarastce@mail.uc.edu>
2020-09-16 17:04:17 +0000

    implementing the Generalized Dice Loss

--- Core/loss/loss.py
+++ Core/loss/loss.py
@@ -10,3 +10,19 @@ def categorical_cross_entropy(y_pred, y_true):
   weights[mask] = y_true[:,mask].shape[0]/(y_true.shape[1]*class_sum[mask])
   weighted_loss = weights[None,:] * loss
   return weighted_loss.sum(dim=1).mean()
+
+
+def generalized_dice(y_pred, y_true):
+  weights = torch.zeros(y_true.shape[1]).to(y_true.device)
+  class_sum = y_true.sum(dim=0)
+  mask = (class_sum>0)
+  weights[mask] = 1/y_true[:,mask].sum(dim=0)**2
+  num = y_pred*y_true
+  num = (weights*num)[:,mask].sum(dim=0)
+  den = (y_pred + y_true)
+  den = (weights*den)[:,mask].sum(dim=0)
+  
+  
+  dice_coef = 2*num.sum()/den.sum()
+  val = 1 -dice_coef
+  return val

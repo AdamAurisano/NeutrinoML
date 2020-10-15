e6976aaffd1efe6a9c04bc71c6c90b8bbd874ea6
jeremy hewes <jhewes15@fnal.gov>
2020-10-15 11:30:54 -0400

    Added jupyter port to log file, to avoid log files for different workflows colliding

--- scripts/run_jupyter.sh
+++ scripts/run_jupyter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup jupyter lab --port=${JUPYTER_PORT} > /scratch/logs/jupyter.log 2>&1 &
+nohup jupyter lab --port=${JUPYTER_PORT} > /scratch/logs/jupyter_${JUPYTER_PORT}.log 2>&1 &
 

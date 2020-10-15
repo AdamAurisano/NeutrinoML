9ef0854214a8395cff1a0709b3329ec98114fda2
jeremy hewes <jhewes15@fnal.gov>
2020-10-15 11:32:01 -0400

    Updated Minkowski data arrange function to ship coordinate tensor to GPU, since this is now a requirement in MinkowskiEngine v0.5

--- Core/utils/arrange_data.py
+++ Core/utils/arrange_data.py
@@ -10,9 +10,9 @@ def arrange_sparse_minkowski(data, device):
 
 def arrange_sparse_minkowski_2stack(data, device):
     batch_input = [ data['sparse'][0].to(device),
-                    data['sparse'][1],   
+                    data['sparse'][1].to(device),   
                     data['sparse'][2].to(device),
-                    data['sparse'][3] ]
+                    data['sparse'][3].to(device) ]
     return batch_input
 
 def arrange_graph(data, device):

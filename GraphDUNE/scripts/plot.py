87e3e6bc2afd2f90bac5b05e83ab600bd794a5c2
jeremy hewes <jhewes15@fnal.gov>
2020-11-05 16:35:04 -0500

    updating plot and inference tools for DUNE graph workflow

---
+++ GraphDUNE/scripts/plot.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+# Import modules
+import os, sys, yaml, argparse, logging, numpy as np, tqdm
+import matplotlib.pyplot as plt
+import torch, torch_geometric
+if '/scratch' not in sys.path: sys.path.append('/scratch')
+from GraphDUNE import datasets
+from GraphDUNE.plot import graphplot
+from Core.trainers import Trainer
+from torch_geometric.data import DataLoader
+
+# Configuration options
+def configure(config):
+  '''Load input configuration file'''
+  with open(config) as f:
+    return yaml.load(f, Loader=yaml.FullLoader)
+
+# Configuration options (overwrite default configuration with your own if you want!)
+parser = argparse.ArgumentParser('plot.py')
+add_arg = parser.add_argument
+add_arg('config', nargs='?', default='/scratch/GraphDUNE/config/hit2d.yaml')
+args = parser.parse_args()
+config = configure(args.config)
+
+full_dataset = datasets.get_dataset(**config['data'])
+trainer = Trainer(**config['trainer'])
+
+fulllen = len(full_dataset)
+tv_num = np.ceil(fulllen*config['data']['t_v_split'])
+splits = np.cumsum([fulllen-tv_num,0,tv_num])
+
+# Load dataset
+valid_dataset = torch.utils.data.Subset(full_dataset,np.arange(start=splits[1],stop=splits[2]))
+
+# Build model
+trainer.build_model(**config['model'])
+trainer.load_state_dict(config['test']['state_dict'])
+
+# Draw plots
+name = config['trainer']['train_name']
+if not os.path.exists(f'plots/{name}'): os.makedirs(f'plots/{name}')
+for i in tqdm.tqdm(range(100)):
+  graph = valid_dataset[i]
+  graphplot.plot_edge_truth(graph)
+  plt.savefig(f'plots/{name}/graph_{i:04d}_truth.pdf')
+  plt.close()
+  graphplot.plot_edge_score(trainer, graph)
+  plt.savefig(f'plots/{name}/graph_{i:04d}_score.pdf')
+  plt.close()
+  graphplot.plot_edge_diff(trainer, graph)
+  plt.savefig(f'plots/{name}/graph_{i:04d}_diff.pdf')
+  plt.close()
+

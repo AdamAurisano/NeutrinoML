89ad5ca9838aa50e71aab306d04b2e799d3d59f7
jeremy hewes <jhewes15@fnal.gov>
2021-01-22 15:05:52 -0500

    Updated recipe for graph network docker image

--- docker/install_packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-conda install pytorch -c pytorch
-export CUDA_HOME=/usr/local/cuda
-pip install -U git+https://github.com/NVIDIA/MinkowskiEngine -v --no-deps
-conda install -c conda-forge pytorch_geometric
-conda install PyYAML tensorboard
-pip install parameter-sherpa
-

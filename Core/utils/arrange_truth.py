d940bf8c19ef67da2418217bdf6405bd79cb2084
jeremy hewes <jhewes15@fnal.gov>
2021-02-09 22:32:08 -0500

    Restoring arrange_sparse function that was renamed, since NOvA workflow also uses it.

--- Core/utils/arrange_truth.py
+++ Core/utils/arrange_truth.py
@@ -1,17 +1,21 @@
 import torch
 
+def arrange_sparse(data):
+    """Arrange ground truth for a batch of sparse pixel maps"""
+    return data["y"]
+
 def sparse_semantic_truth(data):
-    '''Arrange  ground truth for a batch of sparse pixel maps'''
-    return data['y']
+    """Arrange semantic segmentation ground truth for a batch of sparse pixel maps"""
+    return data["y"]
 
 def sparse_instance_truth(data):
-    '''Arrange  ground truth for a batch of sparse pixel maps'''
-    return data['chtm']
+    """Arrange instance segmentation ground truth for a batch of sparse pixel maps"""
+    return data["chtm"]
 
 def arrange_dense(data):
-    '''Arrange ground truth for a batch of sparse pixel maps'''
-    return data['y']
+    """Arrange ground truth for a batch of sparse pixel maps"""
+    return data["y"]
 
 def arrange_graph(data):
-    '''Arrange ground truth for a batch of graphs when using DataParallel'''
+    """Arrange ground truth for a batch of graphs when using DataParallel"""
     return data.y

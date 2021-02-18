9a9d67d4749c0e90b0faa0f020a42b38a2141f50
CarlosSarasty <sarastce@mail.uc.edu>
2021-02-18 16:12:37 +0000

    Updated sparse_panoptic_truth function

--- Core/utils/arrange_truth.py
+++ Core/utils/arrange_truth.py
@@ -8,9 +8,10 @@ def sparse_semantic_truth(data):
     """Arrange semantic segmentation ground truth for a batch of sparse pixel maps"""
     return data["y"]
 
-def sparse_instance_truth(data):
+def sparse_panoptic_truth(data):
     """Arrange instance segmentation ground truth for a batch of sparse pixel maps"""
-    return data["chtm"]
+    ret ={'sem_seg':data["y"], 'ctr_htm':data["chtm"], 'offset':data['offset']} 
+    return ret 
 
 def arrange_dense(data):
     """Arrange ground truth for a batch of sparse pixel maps"""

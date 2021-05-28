41f00f489b738fdc8efd05f28d66f04990cf75b3
jeremy hewes <jhewes15@fnal.gov>
2021-05-28 16:23:50 -0400

    Revert "update NOvA dataset names and fix dense dataset"
    This reverts commit 1f3eb79d5a06bdff9018df7774240e4d6e48f50a.

--- SparseNOvA/datasets/spm_nova_sparse_to_dense.py
+++ SparseNOvA/datasets/spm_nova_sparse_to_dense.py
@@ -5,28 +5,29 @@ PyTorch data structure for sparse pixel maps
 
 from torch.utils.data import Dataset
 import os.path as osp, glob, h5py, tqdm, numpy as np, torch
-import awkward as ak
+import random
 import MinkowskiEngine as ME
 from Core import utils
 
 class DensePixelMapNOvA(Dataset):
     def __init__(self, filelist, apply_jitter, **kwargs):
         '''Initialiser for SparsePixelMapNOvA class'''
-        self.data = ak.from_parquet('/data/mp5/cvnmap.parquet', lazy=True)
-
+        self.files = filelist
+        self.apply_jitter = apply_jitter
+        
     def __len__(self):
-        return len(self.data)
+        return len(self.files)
 
     def __getitem__(self, idx):
         '''Return training information at provided index'''
         if not 0 <= idx < len(self):
             raise Exception(f'Event number {idx} invalid – must be in range 0 -> {len(self)-1}.')
 
-        data = { field: torch.from_numpy(ak.to_numpy(self.data[idx][field])) for field in self.data.fields }
-        data['xfeats'] = data['xfeats'].type(torch.float32)
-        data['xcoords'] = data['xcoords'].type(torch.int32)
-        data['yfeats'] = data['yfeats'].type(torch.float32)
-        data['ycoords'] = data['ycoords'].type(torch.int32)
+        data = torch.load(self.files[idx])
+        if self.apply_jitter:
+            scale = random.gauss(1, 0.1)
+            data['xfeats'] *= scale
+            data['yfeats'] *= scale
 
         xview = torch.sparse.FloatTensor(data['xcoords'].long().T, data['xfeats'],
                                          torch.Size([100,80,1])).to_dense().permute(2,0,1)

a996c2670d0ff4383e41b42401364950d6d7d9bc
jeremy hewes <jhewes15@fnal.gov>
2021-07-09 16:57:49 -0400

    update docker recipe
    adding seaborn as a docker dependency, making sure packages are cloned to /usr/local so they work in containers without root privileges, and cleaning up the conda script from the previous stage

--- docker/install_packages.sh
+++ docker/install_packages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+rm /home/Miniconda3-latest-Linux-x86_64.sh
 conda install -y pytorch=1.9.0 -c pytorch
 export CUDA_HOME=/usr/local/cuda
-pip install parameter-sherpa uproot Particle awkward pyarrow tensorboard-data-server boost_histogram
+pip install parameter-sherpa uproot Particle awkward pyarrow tensorboard-data-server boost_histogram seaborn
 pip install -U git+https://github.com/NVIDIA/MinkowskiEngine -v --no-deps
 conda install -y PyYAML tensorboard h5py mpi4py psutil jupyterlab numba
 conda config --set changeps1 False
@@ -16,7 +16,9 @@ pip install torch-geometric
 mkdir /root/.jupyter
 cp /scratch/jupyter_notebook_config.py /root/.jupyter
 export OMP_NUM_THREADS=16
-cd /home
+export PYTHONPATH=/scratch:/usr/local/pandana:/usr/local/NOvAPandAna:/usr/local/pynuml
+cd /usr/local
+git clone https://github.com/jethewes/pynuml
 git clone https://github.com/HEPonHPC/pandana
-export PYTHONPATH=/home/pandana
+git clone https://github.com/grohmc/NOvAPandAna
 

e07f3adfbf903f7cc40271a68fd13c6594e321e3
jeremy hewes <jhewes15@fnal.gov>
2021-07-16 18:10:07 -0400

    update docker recipe
    add a few useful utilities to the docker image recipe - ack, htop, ncdu, net-tools for netstat

--- docker/setup_conda.sh
+++ docker/setup_conda.sh
@@ -2,7 +2,7 @@
 
 apt update
 apt upgrade -y
-apt install -y wget vim git libopenblas-dev
+apt install -y wget vim git libopenblas-dev ack htop ncdu net-tools
 wget -P /home https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash /home/Miniconda3-latest-Linux-x86_64.sh
 

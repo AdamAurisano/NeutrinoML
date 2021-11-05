0b79fa6339984f1b11458eda2dbd5c28097e8577
jeremy hewes <jhewes15@fnal.gov>
2021-11-05 16:18:12 -0400

    update DUNE processing script
    modify script to use new pynuml processing with parallel IO developed by Northwestern group

--- GraphDUNE/scripts/process.py
+++ GraphDUNE/scripts/process.py
@@ -1,50 +1,28 @@
 #!/usr/bin/env python
 
-if __name__ == "__main__":
-  import sys, os, os.path as osp
-  for p in [ "/scratch", "/numl"]:
-    if p not in sys.path: sys.path.append(p)
-  import argparse, numl, glob, multiprocessing as mp
-  from numl.core.out import *
-  from numl.graph.edges import *
-  from functools import partial
+import sys, os.path as osp
+sys.path.append(osp.dirname(osp.dirname(osp.realpath(__file__))))
+import argparse, numl
+
+def main():
 
   args = argparse.ArgumentParser()
-  args.add_argument("-i", "--input", type=str, required=True,
-                    help="input directory to read files from")
-  args.add_argument("-o", "--output", type=str, required=True,
-                    help="output directory to write files to")
-  args.add_argument("-p", "--processes", type=int, default=None,
-                    help="number of threads to parallelise over")
-  args.add_argument("-h5", "--hdf5", default=False, action="store_true",
-                    help="write output to HDF5 file instead of PyTorch files")
+  args.add_argument("-i", "--infile", type=str, required=True,
+                    help="input HDF5 file")
+  args.add_argument("-o", "--outfile", type=str, required=True,
+                    help="output file name")
+  args.add_argument("-5", "--hdf5", action="store_true",
+                    help="write output to HDF5 files")
+  args.add_argument("-p", "--profile", action="store_true",
+                    help="run timing profile")
+  args.add_argument("-s", "--sequence", action="store_true",
+                    help="use sequencing")
   opts = args.parse_args()
 
-  files = glob.glob("{}/*.h5".format(opts.input))
-  if not osp.exists(opts.output): os.mkdir(opts.output)
+  out = numl.core.out.H5Out(opts.outfile) if opts.hdf5 else numl.core.out.PTOut(opts.outfile)
+  numl.process.hitgraph.process_file(out, opts.infile, l=numl.labels.standard.panoptic_label, \
+                                     use_seq=opts.sequence, profile=opts.profile)
 
-  if opts.hdf5: out = H5Out(osp.join(opts.output, "out.h5"))
-  else: out = PTOut(opts.output)
-
-  kwargs = {
-    "out": out,
-    "g": numl.process.hitgraph.process_event,
-    # "g": numl.process.hitgraph.process_event_singleplane,
-    "l": numl.labels.standard.panoptic_label,
-    # "l": numl.labels.ccqe.semantic_label,
-    "e": numl.graph.edges.delaunay,
-    # "e": numl.graph.edges.radius,
-  }
-
-  if opts.processes is not None:
-    from functools import partial
-    func = partial(numl.process.hitgraph.process_file, **kwargs)
-    import multiprocessing as mp
-    with mp.Pool(processes=opts.processes) as pool:
-      pool.map(func, files)
-  else:
-    numl.process.hitgraph.process_file(files[1], **kwargs)
-    # for f in files: numl.process.hitgraph.process_file(f, **kwargs
-
-  print("done processing files!")
+if __name__ == "__main__":
+   main()
 
